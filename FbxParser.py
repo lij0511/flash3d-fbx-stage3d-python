98e596f12636cbe4df54e7c86d19cac4f57605bd
BobLChen <chenbo150928@gmail.com>
2014-11-27 09:37:23 +0800

    fix bug
    TODO:绑定点数据导出。

--- FbxParser.py
+++ FbxParser.py
@@ -112,19 +112,19 @@ def parseArgument():
 
     parser = argparse.ArgumentParser()
     # 解析法线
-    parser.add_argument("-normal",  help = "parse normal",      action = "store_true",      default = True)
+    parser.add_argument("-normal",  help = "parse normal",      action = "store_true",      default = False)
     # 解析UV0
-    parser.add_argument("-uv0",     help = "parse uv0",         action = "store_true",      default = True)
+    parser.add_argument("-uv0",     help = "parse uv0",         action = "store_true",      default = False)
     # 解析UV1
-    parser.add_argument("-uv1",     help = "parse uv1",         action = "store_true",      default = True)
+    parser.add_argument("-uv1",     help = "parse uv1",         action = "store_true",      default = False)
     # 解析动画
-    parser.add_argument("-anim",    help = "parse animation",   action = "store_true",      default = True)
+    parser.add_argument("-anim",    help = "parse animation",   action = "store_true",      default = False)
     # 使用全局坐标
-    parser.add_argument("-world",   help = "world Transofrm",   action = "store_true",      default = True)
+    parser.add_argument("-world",   help = "world Transofrm",   action = "store_true",      default = False)
     # 指定Fbx文件路径
     parser.add_argument("-path",    help = "fbx file path  ",   action = "store",           default = "")
     # 使用四元数方式
-    parser.add_argument("-quat",    help = "quat with anima",   action = "store_true",      default = True)
+    parser.add_argument("-quat",    help = "quat with anima",   action = "store_true",      default = False)
     # 使用四元数时，最大骨骼数
     parser.add_argument("-max_quat",help = "bone num with quat",action = "store",           default = 56)
     # 使用矩阵时，最大骨骼数
@@ -1327,10 +1327,6 @@ if __name__ == "__main__":
     # 解析参数
     config = parseArgument()
     fbxList = scanFbxFiles(config.path)
-#     fbxList = ["/Users/Neil/python/ImportSceneSDK2015/test/yasuo.FBX"]
-#     fbxList = ["/Users/Neil/python/ImportSceneSDK2015/test/nvhai.fbx"]
-#     fbxList = ["/Users/Neil/python/ImportSceneSDK2015/test/ship/ship.fbx"]
-    fbxList = ["/Users/Neil/python/ImportSceneSDK2015/test/Test22.FBX"]
     
     for item in fbxList:
         parseFBX(item, config)
